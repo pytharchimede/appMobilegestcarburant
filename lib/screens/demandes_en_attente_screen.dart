22b220dfbbfecf2a6031c93b3771aa62fa05a105
pytharchimede <amani_ulrich@outlook.fr>
2025-07-13 19:51:08 +0000

    fix demande carburant

--- lib/screens/demandes_en_attente_screen.dart
+++ lib/screens/demandes_en_attente_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 import '../services/api_services.dart'; // Assure-toi d'importer ton ApiService
 
 class DemandesEnAttenteScreen extends StatefulWidget {
